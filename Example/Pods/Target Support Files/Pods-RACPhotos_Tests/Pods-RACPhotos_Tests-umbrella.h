d777ff9194a1aa6c01a7457c72b14f6a495d403f
Alejandro Martínez <alexito4@gmail.com>
2015-08-21 14:54:37 +0100

    Remove Pods folder

--- Example/Pods/Target Support Files/Pods-RACPhotos_Tests/Pods-RACPhotos_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RACPhotos_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RACPhotos_TestsVersionString[];
-
--- Example/Pods/Target Support Files/RACPhotos/RACPhotos-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RACPhotos : NSObject
-@end
-@implementation PodsDummy_RACPhotos
-@end
